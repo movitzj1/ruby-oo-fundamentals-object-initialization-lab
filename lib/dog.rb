1aefcddbe44d4975808f590dc82e98883edb2e11
Justin Movitz <movitzj1@montclair.edu>
2021-01-28 14:34:24 -0500

    commit tree 20dd2abc76816dc18aa831b307fb0201da95c8cc

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,22 @@
 class Dog
-    def initialize(name, breed = "Mutt")
-      @name = name
-      @breed = breed
-    end
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
   
-    def name=(name)
-      @name = name
-    end
+  def name=(name)
+    @name = name
+  end
   
-    def name
-      @name
-    end 
+  def name
+    @name
+  end 
       
-    def breed=(breed)
-        @breed = breed
-    end
+  def breed=(breed)
+    @breed = breed
+  end
       
-    def breed
-        @breed
-    end
+  def breed
+    @breed
+  end
 end
